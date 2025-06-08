9e87528a358bddb11fb4ec9b3cc742d5fd7cb4de
Nazmol3215 <mdasikahamad2006@gamil.com>
2025-06-08 17:11:01 +0600

    Added mistri profale

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://smart-bhaluka.vercel.app', // আপনার লাইভ সাইট URL
+  generateRobotsTxt: true, // robots.txt ফাইলও তৈরি করবে
+};
